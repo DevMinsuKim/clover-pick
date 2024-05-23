393500bef6525c1299dc6666fd68a8ec7e8bbf6f
Minsu Kim <70056622+DevMinsuKim@users.noreply.github.com>
2024-05-23 17:22:01 +0900

    refactor: 전체 코드 변경

--- src/app/components/common/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <div className="text-center w-full">
-      <p className="text-xs py-5 xl:py-10">ⓒ SMOTTO All rights reserved.</p>
-    </div>
-  );
-}
