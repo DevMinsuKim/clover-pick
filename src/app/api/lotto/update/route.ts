b589971447065d9d8e0ab8e69b66b524d7f1a1aa
Minsu Kim <70056622+DevMinsuKim@users.noreply.github.com>
2024-06-28 10:21:47 +0900

    feat: 로또 및 연금복권 데이터 캐시 무효화 로직 추가

--- src/app/api/lotto/update/route.ts
+++ src/app/api/lotto/update/route.ts
@@ -5,6 +5,8 @@ import * as Sentry from "@sentry/nextjs";
 import iconv from "iconv-lite";
 import prisma from "@/lib/prisma";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   try {
     const url = process.env.LOTTO_DATA_API_URL;
@@ -76,11 +78,8 @@ export async function GET() {
       });
     }
 
-    console.log(data);
-
     return NextResponse.json({ message: true });
   } catch (error) {
-    console.log(error);
     Sentry.captureException(error);
     return NextResponse.json({ message: false }, { status: 500 });
   }
