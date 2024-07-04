ed508e7716aac74e2444e33e3c6b9c961b1f1877
Minsu Kim <70056622+DevMinsuKim@users.noreply.github.com>
2024-07-04 19:37:57 +0900

    feat: 로또 페이지 동적으로 변경

--- src/app/lotto/page.tsx
+++ src/app/lotto/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import LottoGenerator from "@/components/lotto/LottoGenerator";
 import React from "react";
 import ErrorFallback from "@/components/common/ErrorFallback";
--- src/libs/axiosInstance.ts
+++ src/libs/axiosInstance.ts
@@ -5,8 +5,8 @@ function getBaseURL() {
   if (!isServer) {
     return "";
   }
-  if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
-    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
   }
   return "http://localhost:3000";
 }
